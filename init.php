f0b04fdae696a4076c0beb98588662de75a071fc
Vadim Cherepenichev <vadim.cvy@gmail.com>
2023-09-14 00:48:49 +0300

    Update/remove boilerplate files

--- init.php
+++ init.php
@@ -1,9 +1,7 @@
 <?php
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-define( 'JLX_ROOT_PATH', __DIR__ . '/' );
-define( 'JLX_ASSETS_PATH', JLX_ROOT_PATH . 'assets/' );
-define( 'JLX_JS_PATH', JLX_ASSETS_PATH . 'js/dist/' );
-define( 'JLX_CSS_PATH', JLX_ASSETS_PATH . 'css/' );
-
-\Jlx\Example::do_something();
+define( 'PJS_ROOT_PATH', __DIR__ . '/' );
+define( 'PJS_ASSETS_PATH', PJS_ROOT_PATH . 'assets/' );
+define( 'PJS_JS_PATH', PJS_ASSETS_PATH . 'js/dist/' );
+define( 'PJS_CSS_PATH', PJS_ASSETS_PATH . 'css/' );
