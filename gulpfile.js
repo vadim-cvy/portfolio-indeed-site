24350a169eb224b30a8da3289b0c0e9c4353b8aa
Vadim Cherepenichev <vadim.cvy@gmail.com>
2023-11-04 17:09:45 +0200

    Fix gulp css generation

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,27 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename');
 const path = require('path');
 
-const srcDir = 'assets/src/scss';
-const distDir = 'assets/dist/css';
+const srcDir = './assets/src/scss';
+const distDir = './assets/dist/css';
 
-gulp.task('compile-scss', function () {
+gulp.task('scss', () => {
   return gulp
-    .src(path.join(srcDir, '*/index.scss'))
+    .src(path.join(srcDir, '/*/index.scss'))
     .pipe(sass().on('error', sass.logError))
     .pipe(cleanCSS())
-    .pipe(rename({ suffix: '.min' }))
+    .pipe(rename(path =>
+    {
+      path.dirname = path.dirname.split( '\\' ).reverse()[0]
+
+      path.extname = '.min' + path.extname
+    }))
     .pipe(gulp.dest(distDir));
 });
 
-gulp.task('default', gulp.series('compile-scss'));
-
-gulp.task('watch', function () {
-  gulp.watch(path.join(srcDir, '**/*.scss'), gulp.series('compile-scss'));
-});
+gulp.task('watch', () => gulp.watch(
+  srcDir + '/**/*.scss',
+  done => gulp.series(['scss'])(done)
+))
